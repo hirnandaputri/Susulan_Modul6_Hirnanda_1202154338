71e914060f751827d4593ed70e51c70585af3581
Kem <putrihirnanda97@gmail.com>
2018-04-01 19:40:45 +0700

    last

---
+++ app/src/main/java/kem/example/com/hirnandaputri_1202154338_modul6/model/User.java
@@ -0,0 +1,29 @@
+package kem.example.com.hirnandaputri_1202154338_modul6.model;
+
+public class User {
+
+    String userId;
+    String username;
+    String email;
+
+    public User() {
+    }
+
+    public User(String userId, String username, String email) {
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
